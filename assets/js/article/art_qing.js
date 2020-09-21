9a80b62f23e58f2acae0a6fff00773707d7a1b55
wuyulin <3186277013@qq.com>
2020-09-21 20:40:33 +0800

    大事件完成成

---
+++ assets/js/article/art_qing.js
@@ -0,0 +1,137 @@
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    var str = location.search
+    var ser = str.split("=")
+    var id = ser[1]
+    initCate()
+    // 初始化富文本编辑器
+    initEditor()
+    var $image = null
+    var options = null
+    // 定义加载文章分类的方法
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('初始化文章分类失败！')
+                }
+                // 调用模板引擎，渲染分类的下拉菜单
+                var htmlStr = template('tpl_cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                // 一定要记得调用 form.render() 方法
+                form.render()
+                fn()
+            }
+        })
+    }
+    function fn() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/' + id,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章失败')
+                }
+                form.val('form-edit', res.data)
+                $('#image').prop('src',)
+                // 1. 初始化图片裁剪器
+                $image = $('#image')
+
+                // 2. 裁剪选项
+                options = {
+                    aspectRatio: 400 / 280,
+                    preview: '.img-preview'
+                }
+
+                // 3. 初始化裁剪区域
+                $image.cropper(options)
+            }
+        })
+    }
+
+
+
+
+
+    // 为选择封面的按钮，绑定点击事件处理函数
+    $('#btnChooseImage').on('click', function () {
+        // console.log(11);
+        $('#coverFile').click()
+    })
+
+    // 监听 coverFile 的change 事件，获取用户选择的文件列表
+    $('#coverFile').on('change', function (e) {
+        // 获取到文件的列表数组
+        // console.log(e);
+        var files = e.target.files
+        // console.log(files);
+        // 判断用户是否选择了文件
+        if (files.legth === 0) {
+            return
+        }
+        // 根据文件，创建对应的 URL 地址
+        var newImgURL = URL.createObjectURL(files[0])
+
+        // 为裁剪区域重新设置图片
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+
+
+    // 定义文章的默认发布状态
+    var art_state = '已发布'
+
+    // 为存为草稿的按钮  绑定点击事件处理函数
+    $('#btnSave2').on('click', function () {
+        art_state = '草稿'
+    })
+    // console.log(1111);
+    // 为表单绑定 subimt事件
+    $('#form-pub').on('submit', function (e) {
+        // 1.阻止表单的默认提交行为
+        e.preventDefault()
+        // 2基于form表单，快速创建一个FormData 对象
+        var fd = new FormData($(this)[0])
+        fd.append('state', art_state)
+
+        $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) {
+                // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                // 5. 将文件对象，存储到 fd 中
+                fd.append('cover_img', blob)
+                // 6. 发起 ajax 数据请求
+                publishArticle(fd)
+            })
+    })
+    function publishArticle(fd) {
+        fd.append("Id", id)
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/edit',
+            data: fd,
+            // 注意：如果向服务器提交的是 FormData 格式的数据，
+            // 必须添加以下两个配置项
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('修改文章失败！')
+                }
+                // layer.msg('修改文章成功！')
+                // 发布文章成功后，跳转到文章列表页面
+                location.href = '/article/art_list.html'
+            }
+        })
+    }
+})
